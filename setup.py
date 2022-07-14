1a63e805b5d6b17a0caec1755147c46d0156cc3e
yutengjin <109270583+yutengjin@users.noreply.github.com>
2022-07-14 01:48:32 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gym_crack_v20_1',
+      version='0.0.1',
+      description='Fatigue crack environment for reinforcement learning',
+      author='Yuteng Jin',
+      author_email='jin001@tamu.edu',
+      install_requires=['gym']
+)
